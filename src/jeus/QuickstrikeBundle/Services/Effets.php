5135d742e5af95e77e0679e7a788e2188c92ae5c
smagghe <jsmagghe@gmail.com>
2015-05-13 07:29:51 +0200

    effets

--- src/jeus/QuickstrikeBundle/Services/Effets.php
+++ src/jeus/QuickstrikeBundle/Services/Effets.php
@@ -11,6 +11,37 @@ use Doctrine\Common\Persistence\ObjectManager;
  */
 class Effets
 {
+    private $CarteEnJeus;
+
+    public function chargerCarteEnJeu($CarteEnJeus) {
+        $this->CarteEnJeus = $CarteEnJeus;
+    }
+
+    public function bonusAttaque($numeroAttaquant,$numeroDefenseur,$infos) {
+        $bonus = 0;
+        $CarteEnJeus = $this->CarteEnJeus[$numeroAttaquant]['ACTIVE'];
+        foreach ($CarteEnJeus as $Cartejeu) {
+            $Carte = $Cartejeu->getCarte();
+            if ($Carte == null) {
+                continue;
+            }
+        }
+
+        return $bonus;
+    }
+
+    public function bonusDefense($numeroDefenseur,$numeroAttaquant,$infos) {
+        $bonus = 0;
+        $CarteEnJeus = $this->CarteEnJeus[$numeroDefenseur]['ACTIVE'];
+        foreach ($CarteEnJeus as $Cartejeu) {
+            $Carte = $Cartejeu->getCarte();
+            if ($Carte == null) {
+                continue;
+            }
+        }
+
+        return $bonus;
+    }
 
 
 
